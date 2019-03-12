f4d65b2a62d5cd998bc078c3856e3d5d46970576
Eli Mason <02eli.mason@gulllakecs.org>
2019-03-12 09:33:25 -0400

    Updated Final Project V2

--- FinalProject.py
+++ FinalProject.py
@@ -2,15 +2,24 @@
 # Final Project
 # Soccer Shot Percentage
 
-total = 0
-
 name = input('What is your name?: ')
+
+def greeting():
+    print('Hello, ' + name + '!')
+    print('')
+greeting()
+
+total = 0
+totals = 0
+
 how_many_games = int(input( '\n' 'How many games would you like to Average?: '))
 
 for i in range(how_many_games):
     num_shots = int(input( '\n' 'How many shots did you take this game?: '))
+    num_shots_made = int(input('How many shots did you make this game?: '))
     total = total + num_shots
+    totals = totals + num_shots_made
 
-average = total / how_many_games
+average = total / totals
 
 print('Shot Percentage: ' + str(round(average, 2)))
